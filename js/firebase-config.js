9ddfc0867105734e64a4cb5abe5ff39d5467cdf1
Pujan Shakya <puzann9843318452@gmail.com>
2023-06-15 15:22:43 -0700

    Added Cdn Files in index.html

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCp-fSR5TCp7gMZGO96DHLKtaJLISFsQdQ",
+  authDomain: "favio-6345f.firebaseapp.com",
+  projectId: "favio-6345f",
+  storageBucket: "favio-6345f.appspot.com",
+  messagingSenderId: "58244825457",
+  appId: "1:58244825457:web:b7e9404f0df998dc10b6d0",
+  measurementId: "G-1F3R2JMW0L"
+  };
+
+
+  firebase.initializeApp(firebaseConfig);
