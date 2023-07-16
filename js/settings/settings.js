a09c4b3e7b81eb2dbb34703b40b436a2a26af35f
thalhauzair1 <135381947+thalhauzair1@users.noreply.github.com>
2023-07-16 01:32:43 -0700

    #32 favio fixed bug of availability button  and added html and css for different pages

---
+++ js/settings/settings.js
@@ -0,0 +1,15 @@
+function logoutFunc(e){
+    console.log("logout");
+    e.preventDefault();
+    firebase.auth().signOut()
+        .then(() => {
+            // Clear session storage or any other necessary cleanup
+            sessionStorage.clear();
+            alert("You have signed out successfully!");
+            // Redirect to the login page or any other desired destination
+            window.location.href = "./../../index.html";
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
