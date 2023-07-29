fca126f0c54776d9899c6a62df45386a4f34f348
Pujan Shakya <puzann9843318452@gmail.com>
2023-07-28 23:02:06 -0700

    sw offline

--- js/registration/login.js
+++ js/registration/login.js
@@ -35,18 +35,31 @@ signup.addEventListener('click', async(e) => {
     window.location.href = "./../../pages/registration/signup.html";
 });
 
+
+
+  // Register Service Worker when window load event happen
 window.addEventListener('load', async () => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-        .register('./../../sw.js')
-        .then(function (reg) {
-          console.log(`Service Worker Registered`);
-        })
-        .catch(function (error) {
-          console.log(`Service Worker Error (${error})`);
-        });
-    } else {
-      console.error("This browser doesn't support Service Worker");
-    }
-  });
+  // does the browser support service workers?
+  if ('serviceWorker' in navigator) {
+      // then register our service worker
+    try {
+      const reg = await  navigator.serviceWorker.register('./../../sw.js', { scope: '/' });
+      // display a success message
+      console.log(`Service Worker is Registered:`, reg);
+    } catch(error) {
+      // display an error message
+      console.log(`Service Worker Error (${error})`);
+    };
+
+    // ready is a Promise that never rejects and resolved when the service worker is active. 
+    const active = await navigator.serviceWorker.ready;
+    // we have an active service worker working for us
+    console.log(`Service Worker is Active:`, active);
+    // At this point, you can call methods that require an active service worker,
+    // like registration.pushManager.subscribe() for push notification
+  } else {
+    // happens when the app isn't served over a TLS connection (HTTPS)
+    console.warn('Service Worker is not available');
+  }
+})
   
--- sw.js
+++ sw.js
@@ -1,61 +1,55 @@
-// self.addEventListener('fetch', event => {
-//     // Fires whenever the app requests a resource (file or data)  normally this is where the service worker would check to see
-//     // if the requested resource is in the local cache before going to the server to get it. 
-//     console.log(`[SW] Fetch event for ${event.request.url}`);
+const cacheName = "v1";
+const urlsToCache = [ "offline.html",
+ ];
+// NEVER cache service worker itself ( i.e. don't include sw.js in above array)
+// MAKE SURE THERE IS NO TYPO in the File names otherwise the cache.addAll fails in install
 
-// });
+self.addEventListener('install', (event) => { // invoked when a browser installs this SW
+    // here we cache the resources that are defined in the urlsToCache[] array
+    console.log(`[SW] Event fired: ${event.type}`);
+    event.waitUntil(       // waitUntil tells the browser to wait for the input promise to finish
+		  caches.open( cacheName )		//caches is a global object representing CacheStorage
+			  .then( ( cache ) => { 			// open the cache with the name cacheName*
+				  return cache.addAll( urlsToCache );      	// pass the array of URLs to cache**. it returns a promise
+		}));
+    self.skipWaiting();  // it tells browser to activate this SW and discard old one immediately (useful during development)
+    console.log(`[SW] installed`);
+});
+
+self.addEventListener('activate', (event) => { // invoked after the SW completes its installation. 
+    // It's a place for the service worker to clean up from previous SW versions
+    console.log(`[SW] Event fired: ${event.type}`);
+    event.waitUntil( deleteOldCache() )    // waitUntil tells the browser to wait for the input promise to finish
+    self.clients.claim(); // lets the newly activated SW takes control of all related open pages 
+    console.log(`[SW] activated`);
+});
+
+// iterates over cache entries for this site and delete all except the one matching cacheName
+async function deleteOldCache() {
+  const keyList = await caches.keys();
+  return Promise.all( keyList.map( ( key ) => {
+    if ( key !== cacheName  ) {    // compare key with the new cache Name in SW
+      return caches.delete( key );  // delete any other cache
+    }
+  }));
+}
 
 
-'use strict';
-console.log("pwa");
 
-const cacheName = 'pwa-offline-v1';
-const contentToCache = ['./pages/offline.html'];
-
-self.addEventListener('fetch', function( event ) {
-    //console.log(`Fetching ${event.request.url}`);  
-      event.respondWith(
-        (async() => {
-          try{
-            const preload = await event.preloadResponse;
-            if (preload) {
-              return preload;
-            }
-
-            const originalResponse = await fetch(event.request);
-            return originalResponse;
-          }catch(error){
-                  let cache = await caches.open(cacheName);
-                  let response = await cache.match('./pages/offline.html');
-
-                  return response;
-          }
-
-        })()
-        
-      );
-
+self.addEventListener('fetch', event => {
+     // do nothing here, just log all the network requests
+    console.log(`Fetching ${event.request.url}`);
+    // generally the service worker caching strategy is defeind here
+    event.respondWith(NetworkFirstOrDefaultPage(event) );
 });
 
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-     (async () => {
-        let cache = await caches.open(cacheName);
-        await cache.addAll(contentToCache);
-      })()
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', function( event ) {
-  event.waitUntil(
-    (async () => {
-      if ("navigationPreload" in self.registration) {
-        await self.registration.navigationPreload.enable();
-      }
-    })()
-  );
-
-  self.clients.claim();
-});
+// NETWORK FIRST, THEN CACHE STRATEGY
+async function NetworkFirstOrDefaultPage(event) {
+    try {
+        return await fetch( event.request );  // returns server fetch
+    } catch(error) {
+        console.log(error);
+        return caches.match( 'offline.html' , {ignoreVary:true} ); // returns default offline page)
+    }
+  }
