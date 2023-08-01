c2eb7fdd1d69cd941243121159a0bef8f214e986
Pujan Shakya <puzann9843318452@gmail.com>
2023-08-01 16:53:42 -0700

    Hamburger shown in request page

--- js/template/navigation.js
+++ js/template/navigation.js
@@ -1,30 +1,16 @@
+// alert('Navigation');
 const btnMain = document.querySelector('.hamburger');
 
 const body = document.body;
 
 btnMain.addEventListener('click', () => {
+    console.log('hamburger selected');
     document.body.classList.toggle('menu-mobile-active')
 })
 
 
 
 
-
-const firsturl =  sessionStorage.getItem("firsturl");
-
-function navigate_back(){
-    const previousPage = document.referrer;
-    console.log(previousPage);
-    const isFromSettingPage = previousPage.includes('settings.html');
-   
-if (isFromSettingPage) {
-    window.location.href = './../../pages/settings/settings.html';
-    } 
-else{
-    window.location.href = firsturl;
-}
-}
-
 function  navigate_back_home(){
-    window.location.href = firsturl;
+    window.location.href = sessionStorage.getItem("firsturl");;
 }
